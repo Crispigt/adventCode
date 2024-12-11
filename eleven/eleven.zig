baf2e6b5be3ea56c4125047b5296538796ac8bff
Felix Stenberg <felixste@kth.se>
2024-12-11 11:34:50 +0100

    Started

--- eleven/eleven.zig
+++ eleven/eleven.zig
@@ -1,7 +1,3 @@
-
-
-
-
 //If num = 0 => 1
 //If num.len % 2 = 0 => split number eg. 10 = 1 and 0, no leading zeroes 00 = 0
 //Else num * 2024
@@ -15,3 +11,56 @@
 // Going with a linked list, then we check each of the things through functions,
 // That is the above functions, also need to implement a linked list, with custom functions
 // to add and remove based on the above
+// Repeat x amount of times
+
+
+// First read input to the linked list
+
+
+const std = @import("std");
+
+fn readLineToVariable(reader: anytype, buffer: []u8) !?[]const u8 {
+    const line = try reader.readUntilDelimiterOrEof(buffer, '\n');
+    return line orelse null; // Handle the case where no more lines are available
+}
+
+fn convertfuckingstringtoint(input: ?[]const u8, allocator: anytype, ) ![]i32 {
+    var ret: i32 = 0;
+
+    if (input) |value| {
+        var ints = try allocator.alloc(i32, value.len);
+        var count: usize = 0;
+        for(value)|item|{
+            const cItem: [1]u8 = [1]u8{item};
+            const trimshit = std.mem.trim(u8, &cItem, " \n\r");
+            ret = try std.fmt.parseInt(i32, trimshit, 10);
+            //std.debug.print("converted: {d}\n", .{ret});
+            ints[count] = ret;
+            count +=1;
+        }
+        return ints;
+    }
+    return &[_]i32{};
+}
+
+
+
+pub fn main() !void {
+
+    const stdin = std.io.getStdIn();
+
+    const allocator = std.heap.page_allocator;
+    var buffer: [1024]u8 = undefined;
+    const reader = stdin.reader();
+    while(true){
+        const line = try readLineToVariable(reader, &buffer);
+        if(line == null) break;
+        //std.debug.print("{any}\n", .{line});
+        const num = try convertfuckingstringtoint(line, allocator); // best thing would be to get the position of each zero from this as well
+        for (num) |value| {
+            std.debug.print("{d} ", .{value});
+        }
+        std.debug.print("\n", .{});
+    } 
+    std.debug.print("\n--- Got input ---\n\n", .{});
+}
