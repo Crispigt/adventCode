33c5845c21ffcee673960b55fc318837b10e94d1
Felix Stenberg <felixste@kth.se>
2024-12-11 11:30:39 +0100

    Added eleven and plan to execute

---
+++ eleven/eleven.zig
@@ -0,0 +1,17 @@
+
+
+
+
+//If num = 0 => 1
+//If num.len % 2 = 0 => split number eg. 10 = 1 and 0, no leading zeroes 00 = 0
+//Else num * 2024
+
+//Maybe a tree would be good, where each node is the number, if split we get two new nodes below,
+//one
+//Otherwise maybe an array where each number has it's own arraylist and we remove then add the splitet number
+//we rebuild the full number by going through and
+//Only problematic part is that we have to shift everything to add a new number inbetween
+//Otherwise a linked list makes it easy to add and remove numbers
+// Going with a linked list, then we check each of the things through functions,
+// That is the above functions, also need to implement a linked list, with custom functions
+// to add and remove based on the above
